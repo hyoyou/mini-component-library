669987d63a5abeed5df058fb3b9eb8c167af762b
Heather You <heather.fswd@gmail.com>
2024-01-18 10:32:20 -0500

    style: add custom styles to native select elemetn

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,74 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+  -webkit-appearance: none; /* Internet Explorer */
+`
+
+const PresentationalBit = styled.div`
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 16px;
+  padding-right: 52px;
+  font-size: 1rem;
+
+  /* The Adjacent Sibling Combinator */
+  /* applies to a button that is the direct sibbling of a div */
+  /* <div> <button> </button> </div>  Applies! */
+  /* <div> <span> <button> </button> </span> </div>  Does not apply! */
+  /* div + button {
+    color: red;
+  } */
+  
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 10px;
+    margin: auto;
+    width: var(--size);
+    height: var(--size);
+    pointer-events: none; /* makes it so that the icon does not receive any click events and block the select */
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
 
